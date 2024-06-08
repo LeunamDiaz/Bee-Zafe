4dda09b4e563cc6aa2b43f7729ab683de0911188
marshallog <leunamdiazmx@gmail.com>
2024-06-08 10:50:17 -0600

    Link error

--- components/Video.js
+++ components/Video.js
@@ -1,6 +1,4 @@
-import Link from 'next/link';
-
-export default function Video() {
+export default function Button({ children, onClick }) {
     return (
       <section
         className="bg-black landing-section h-screen max-w-screen text-center relative overflow-hidden"
@@ -8,22 +6,21 @@ export default function Video() {
       >
         <div className="z-30 relative h-full flex flex-col">
           <header>
-            <h2 className="text-white text-4xl md:text-5xl xl:text-6xl pt-40">
-              Disfrute de Bee Zafe
-            </h2>
+          <h2 className="text-white text-4xl md:text-5xl xl:text-6xl pt-40">
+  Disfrute de Bee Zafe
+</h2>
+
             <p className="text-white text-xl mt-3">
               Conéctate con tus abejas como nunca antes
             </p>
           </header>
           <footer className="flex flex-col flex-grow flex-nowrap justify-end pb-20">
             <div>
-              <Link href="/register">
-                <div>
-                  <button className="text-white border-[3px] border-white rounded font-sm px-12 py-2 bg-white/5 backdrop-blur-sm inline-block hover:bg-white hover:text-black transition-colors">
-                    Programar instalación
-                  </button>
-                </div>
-              </Link>
+            <Link href="/register" className="text-white border-[3px] border-white rounded font-sm px-12 py-2 bg-white/5 backdrop-blur-sm inline-block hover:bg-white hover:text-black transition-colors"
+>
+                <div>Registrarse ahora</div>
+                </Link>
+
             </div>
           </footer>
         </div>
@@ -42,4 +39,5 @@ export default function Video() {
         </div>
       </section>
     );
-}
+  }
+  
